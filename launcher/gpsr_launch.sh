d8a195b567afd4f6951f452f97717815fc5160ef
gollilagollilagollila <b1845350@planet.kanazawa-it.ac.jp>
2020-01-31 14:32:24 +0900

    first commit

---
+++ launcher/gpsr_launch.sh
@@ -0,0 +1,19 @@
+##!/bin/sh
+
+#学内proxyを経由するかどうか確認
+net=$(iwgetid -r)
+if [ "$net" = "KIT-WLAP2" ]; then
+  export https_proxy=http://wwwproxy.kanazawa-it.ac.jp:8080/
+  export http_proxy=http://wwwproxy.kanazawa-it.ac.jp:8080/
+  echo "Changed https_proxy for GCP API"
+else
+  export https_proxy=''
+  export http_proxy=''
+  echo "Cleared http_proxy and https_proxy"
+fi
+
+#仮想環境へ入る
+. ~/catkin_ws/src/basic_fanc/env/bin/activate
+
+#launchファイルを起動
+roslaunch gpsr srvserver.launch
